ee89a43a5c899a588644d400e33834554e10de16
acareaga <aaroncareaga@gmail.com>
2016-02-10 10:28:46 -0700

    Started building helper methods

--- test/block-test.js
+++ test/block-test.js
@@ -4,7 +4,7 @@ var block = require('../lib/block');
 describe('game block', function(){
   var block = new block({});
 
-  it('should assign an x coordinate', function() {
+  xit('should assign an x coordinate', function() {
     assert.equal(block.x, 0);
   });
 });
---
+++ test/board-test.js
@@ -0,0 +1,18 @@
+var assert = require('chai').assert;
+var block = require('../lib/board');
+
+describe('game board', function(){
+  var board = new board({});
+
+  xit('has rows', function() {
+    assert.equal(board.x, 0);
+  });
+
+  xit('has columns', function() {
+    assert.equal(board.x, 0);
+  });
+
+  xit('has a collection of blocks', function() {
+    assert.equal(board.x, 0);
+  });
+});
