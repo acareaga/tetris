dea94dc4cfad709717916252f5a92ec62e8665c2
acareaga <aaroncareaga@gmail.com>
2016-02-10 15:38:35 -0700

    Functions implemented on block to tell if it as the bottom, left, or right side of the board.

--- test/block-test.js
+++ test/block-test.js
@@ -14,21 +14,52 @@ describe('Game Block', function() {
     assert.equal(block.y, 0);
   });
 
-  it('should move down', function(){
+  it('can move down', function(){
     let block = this.board.addBlockToBoard(5, 15);
     block.moveDown()
     assert.equal(block.y, 16);
   });
 
-  it('should move left',function(){
-    let block = this.board.addBlockToBoard(10,15)
+  it('can move left',function(){
+    let block = this.board.addBlockToBoard(10, 15)
     block.moveLeft()
     assert.equal(block.x, 9)
   });
 
-  it('should move right', function(){
-    let block = this.board.addBlockToBoard(10,15)
+  it('can move right', function(){
+    let block = this.board.addBlockToBoard(10, 15)
     block.moveRight()
     assert.equal(block.x, 11 )
   });
+
+  it('should default to active', function(){
+    let block = this.board.addBlockToBoard();
+    assert.equal(block.active, true)
+  });
+
+  it('inactive function changes status of block to false', function(){
+    let block = this.board.addBlockToBoard();
+    assert.equal(block.inactive(), false)
+  });
+
+  it('can tell if the block is at the bottom of the board', function(){
+    let block = this.board.addBlockToBoard(10, 20);
+    assert.equal(block.blockIsAtBottomOfBoard(), true);
+  });
+
+  it('can tell if the block is at the left side of the board', function(){
+    let block = this.board.addBlockToBoard(0, 20);
+    assert.equal(block.blockIsAtLeftSideOfBoard(), true);
+  });
+
+  it('can tell if the block is at the right side of the board', function(){
+    let block = this.board.addBlockToBoard(10, 20);
+    assert.equal(block.blockIsAtRightSideOfBoard(), true);
+  });
+
+  // it('status should change when the block hits the bottom of the grid', function(){
+  //   let block = this.board.addBlockToBoard(10,20);
+  //   assert.equal(block.canMoveDown(), false);
+  // });
+
 });
