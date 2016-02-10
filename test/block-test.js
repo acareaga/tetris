6a18af2b0ce251fa0729968c884fbc1b73e599fb
acareaga <aaroncareaga@gmail.com>
2016-02-10 14:10:57 -0700

    wrote a test for block. block is now able to have blocks on the board. we also created a before action in the test that creates a board we can pass into the block object.

--- test/block-test.js
+++ test/block-test.js
@@ -1,10 +1,19 @@
 var assert = require('chai').assert;
-var block = require('../lib/block');
+var Block = require('../lib/block');
+import Board from '../lib/board';
 
-describe('game block', function(){
-  var block = new block({});
+describe('Game Block', function() {
 
-  xit('should assign an x coordinate', function() {
-    assert.equal(block.x, 0);
+  beforeEach(function () {
+    this.board = new Board();
   });
+
+  it('should default to 0x and 0y', function() {
+
+    let block = this.board.addBlockToBoard(10, 10);
+    assert.equal(block.x, 10);
+    assert.equal(block.y, 10);
+  });
+
+  it('')
 });
--- test/board-test.js
+++ test/board-test.js
@@ -1,8 +1,5 @@
 var assert = require('chai').assert;
-
 import Board from '../lib/board';
-
-var block = require('../lib/block');
 
 describe('Game Board', function(){
 
@@ -14,6 +11,24 @@ describe('Game Board', function(){
   it('has columns', function() {
     let board = new Board(100,0)
     assert.equal(board.columns, 100);
+  });
+
+  it('defaults to a 10x20 board', function(){
+    let board = new Board()
+    assert.equal(board.columns,10)
+    assert.equal(board.rows, 20)
+  });
+
+  it('size can be changed to whatever we want', function(){
+    let board = new Board(1000,10000)
+    assert.equal(board.columns, 1000)
+    assert.equal(board.rows, 10000)
+  });
+
+  xit('rows and columns cannot be negative', function(){
+    let board = new Board(-10, -10);
+    assert.throw(board.colums, 'function throws a reference error')
+    assert.throw(board.rows, 'function throws a reference error')
   });
 
   it('has a collection of blocks', function() {
