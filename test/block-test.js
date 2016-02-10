958f1ed467fcd13915a53018c05119f647b69902
acareaga <aaroncareaga@gmail.com>
2016-02-10 14:50:47 -0700

    added test to the block function. it can now move down left and right. weve also refactored code in the block function to move the block up down left and right

--- test/block-test.js
+++ test/block-test.js
@@ -9,11 +9,26 @@ describe('Game Block', function() {
   });
 
   it('should default to 0x and 0y', function() {
-
-    let block = this.board.addBlockToBoard(10, 10);
-    assert.equal(block.x, 10);
-    assert.equal(block.y, 10);
+    let block = this.board.addBlockToBoard();
+    assert.equal(block.x, 0);
+    assert.equal(block.y, 0);
   });
 
-  it('')
+  it('should move down', function(){
+    let block = this.board.addBlockToBoard(5, 15);
+    block.moveDown()
+    assert.equal(block.y, 16);
+  });
+
+  it('should move left',function(){
+    let block = this.board.addBlockToBoard(10,15)
+    block.moveLeft()
+    assert.equal(block.x, 9)
+  });
+
+  it('should move right', function(){
+    let block = this.board.addBlockToBoard(10,15)
+    block.moveRight()
+    assert.equal(block.x, 11 )
+  });
 });
